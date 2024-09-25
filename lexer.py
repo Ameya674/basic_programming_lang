879593eb7f54c08b618db96523d12451a13c58f5
Ameya Gurjar <ameyaprofessional247@gmail.com>
2024-09-25 13:33:31 -0700

    lexer

---
+++ lexer.py
@@ -0,0 +1,134 @@
+# TOKENS
+INT = "INT"
+FLOAT = "FLOAT"
+PLUS = "PLUS"
+MINUS = "MINUS"
+MUL = "MUL"
+DIV = "DIV"
+LPARENTHESIS = "LPARENTHESIS"
+RPARENTHESIS = "RPARENTHESIS"
+
+#DIGITS
+DIGITS = "0123456789"
+
+#ERRORS
+class Error:
+    
+    def __init__(self,pos_start, pos_end, error_name, details) -> None:
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        self.error_name = error_name
+        self.details = details
+        
+    def error_printing(self) -> str:
+        result = f'{self.error_name}: {self.details}'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+        return result
+    
+class IllegalCharError(Error):
+    
+    def __init__(self, pos_start, pos_end, details) -> None:
+        super().__init__(pos_start, pos_end, "Illegal Character", details)
+        
+#POSITION
+class Position: 
+    
+    def __init__(self, idx, ln, col, fn, fntxt):
+        self.idx = idx
+        self.ln = ln 
+        self.col = col
+        self.fn = fn
+        self.fntxt = fntxt
+        
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+        
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+        
+        return self
+    
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.fntxt)
+
+# TOKEN CLASS
+class Token:
+    
+    def __init__(self, type, value = None) -> None:
+        self.type = type
+        self.value = value
+        
+    def __repr__(self) -> str:
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
+    
+class Lexer:
+    
+    def __init__(self, fn, text) -> None:
+        self.fn = fn
+        self.text = text
+        self.pos = Position(-1, 0, -1, fn, text)
+        self.current_char = None
+        self.advance()
+        
+    def advance(self) -> None:
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+        
+    def make_number(self) -> Token:
+        num_str = ''
+        dot_count = 0
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: 
+                    break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
+        if dot_count == 0:
+            return Token(INT, int(num_str))
+        else:
+            return Token(FLOAT, float(num_str))
+            
+        
+    def make_tokens(self) -> list:
+        tokens = []
+        while self.current_char != None:
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+            elif self.current_char == '+':
+                tokens.append(Token(PLUS))
+                self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(MINUS))
+                self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(MUL))
+                self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(DIV))
+                self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(LPARENTHESIS))
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(RPARENTHESIS))
+                self.advance()
+            else:
+                pos_start = self.pos.copy()
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+                       
+        return tokens, None
+         
+def run(fn, text):
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+    return tokens, error
